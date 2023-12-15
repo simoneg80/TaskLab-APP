6a6fd374b4f270ee7e34c39595d109a6dd80f9e2
Simone Green <simoneg80@yahoo.com>
2023-12-15 13:36:29 -0500

    MERN-Stack Infrastructure

---
+++ controllers/api/users.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
+
+module.exports = {
+  create,
+  login,
+  checkToken,
+
+};
+
+async function create(req, res) {
+  try {
+    console.log(req.body);
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    const token = createJWT(user);
+    res.json(token);
+  } catch(error) {
+    console.log(error);
+    res.status(400).json('Bad Credentials');
+  }
+}
+
+function checkToken(req, res) {
+  // req.user will always be there for you when a token is sent
+  console.log('req.user', req.user);
+  res.json(req.exp);
+}
+
+
+
+//Helper Functions
+function createJWT(user) {
+  return jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
+}
