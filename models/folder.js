1b0baae964da9535ee44a1cc78da6d0e0698b2e8
Simone Green <simoneg80@yahoo.com>
2023-12-18 15:27:35 -0500

    Create folder schema

---
+++ models/folder.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const folderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  // Add more properties as needed
+});
+
+const Folder = mongoose.model("Folder", folderSchema);
+
+module.exports = Folder;
