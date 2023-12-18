513f6d4ed7d57f5fd54e7811e828b2e6b2ff6bea
kiayasimone <simone12017@gmail.com>
2023-12-18 13:51:27 -0500

    create temp state in app.js

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,8 @@
 import TaskListItem from "../TaskListItem/TaskListItem";
 
 export default function TaskList({ tasks }) {
-    const taskListItems = tasks.map((task, idx) => (
-        <TaskListItem task={task} index={idx} key={task} />
-    ));
-    return <ul>{taskListItems}</ul>;
-    
+  const taskListItems = tasks.map((task, idx) => (
+    <TaskListItem task={task} index={idx} key={task} />
+  ));
+  return <ul>{taskListItems}</ul>;
 }
