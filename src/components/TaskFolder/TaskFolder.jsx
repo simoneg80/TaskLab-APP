a93c4e3ab3cad4b35dc5fe604d811159b69ae066
Simone Green <simoneg80@yahoo.com>
2023-12-17 22:45:00 -0500

    Create handleFolderEdit function

--- src/components/TaskFolder/TaskFolder.jsx
+++ src/components/TaskFolder/TaskFolder.jsx
@@ -2,39 +2,68 @@
 import { useState } from "react";
 
 export default function TaskFolder() {
-    const [folderName, setFolderName] = useState("");
-    const [folders, setFolders] = useState([]);
+  const [folderName, setFolderName] = useState("");
+  const [folders, setFolders] = useState([]);
+  const [editIdx, setEditIdx] = useState(-1);
+  const [editedName, setEditedName] = useState("");
 
-    const handleFolderCreation = () => {
-        if(folderName.trim() !== "") {
-            setFolders([...folders, folderName]);
-            setFolderName("");
-        
-
-        }
+  const handleFolderCreation = () => {
+    if (folderName.trim() !== "") {
+      setFolders([...folders, folderName]);
+      setFolderName("");
     }
+  };
+  const handleFolderEdit = (idx) => {
+    setEditIdx(idx);
+    setEditedName(folders[idx]);
+  };
+
+  const handleFolderUpdate = () => {
+    const updatedFolders = [...folders];
+    updatedFolders[editIdx] = editedName;
+    setFolders(updatedFolders);
+    setEditIdx(-1);
+  };
+
+  const handleFolderDelete = (idx) => {
+    const updatedFolders = [...folders];
+    updatedFolders.splice(idx, 1);
+    setFolders(updatedFolders);
+  };
+
   return (
     <>
-    <h1> Task Folder </h1>
-    <div>
-        <input type="text"
-        value={folderName}
-        onChange={(e) => setFolderName(e.target.value)}
-        placeholder="Enter folder name here" 
+      <h1> Task Folder </h1>
+      <div>
+        <input
+          type="text"
+          value={folderName}
+          onChange={(e) => setFolderName(e.target.value)}
+          placeholder="Enter folder name here"
         />
         <button onClick={handleFolderCreation}>Create Folder</button>
-    </div>
+      </div>
 
-    <div>
+      <div>
         {folders.map((folder, idx) => (
-            <div key={idx} className="folder-card">
-            <h3>{folder}</h3>
-            {folder.name}
-            </div>
+          <div key={idx} className="folder-card">
+            {editIdx === idx ? (
+              <input
+                type="text"
+                value={editedName}
+                onChange={(e) => setEditedName(e.target.value)}
+              />
+            ) : (
+              <h3>{folder}</h3>
+            )}
+            <button onClick={() => handleFolderEdit(idx)}>Edit</button>
+            {/* <button onClick={() => handleFolderDelete(idx)}>Delete</button> */}
+            {editIdx === idx && (
+              <button onClick={handleFolderUpdate}>Save</button>
+            )}
+          </div>
         ))}
-    </div>
+      </div>
     </>
   );
 }
-
-//   <ul>{skillListItems}</ul>;
