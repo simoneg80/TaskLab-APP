3bf6346c08a61d85fcb33c5e4acbdce1c7579886
Simone Green <simoneg80@yahoo.com>
2023-12-18 13:54:43 -0500

    Create task api file

--- src/components/NewTaskListForm/NewTaskListForm.jsx
+++ src/components/NewTaskListForm/NewTaskListForm.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 
 export default function NewTaskListForm({ addTaskList }) {
-    const [newForm, setNewForm] = useState({
-        name: "",
-        tasks: []
+  const [newForm, setNewForm] = useState({
+    name: "",
+    tasks: [],
+  });
+
+  function handleChange(evt) {
+    setNewForm({
+      ...newForm,
+      [evt.target.name]: evt.target.value,
     });
-
-    function handleChange(evt) {
-        setNewForm({ 
-            ...newForm, [evt.target.name]: evt.target.value });
-    };
-    function handleSubmit(evt) {
-        evt.preventDefault();
-        addTaskList(newForm);
-        setNewForm({
-            name: "",
-            tasks: []
-        });
-    };
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input
-                    id="name"
-                    name="name"
-                    value={newForm.name}
-                    onChange={handleChange}
-                />
-                <button type="submit"> Add Task List</button>
-            </form>
-        </>
-    );
-
+  }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    addTaskList(newForm);
+    setNewForm({
+      name: "",
+      tasks: [],
+    });
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          value={newForm.name}
+          onChange={handleChange}
+        />
+        <button type="submit"> Add Task List</button>
+      </form>
+    </>
+  );
 }
