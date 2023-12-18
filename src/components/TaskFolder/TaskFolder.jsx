d1817ecd6458ba7676108838ac02b10f975db9e4
Simone Green <simoneg80@yahoo.com>
2023-12-18 00:18:38 -0500

    import css

--- src/components/TaskFolder/TaskFolder.jsx
+++ src/components/TaskFolder/TaskFolder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import TaskList from "../TaskList/TaskList";
+import "./TaskFolder.css";  
 
 export default function TaskFolder() {
   const [folderName, setFolderName] = useState("");
