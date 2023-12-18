99f76872b2f7cd618f6078f3aa211b9850407cde
Josehneves <josehneves28@gmail.com>
2023-12-17 21:46:33 -0500

    fixed profile to display name and email

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
-import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import AuthPage from "../AuthPage/AuthPage";
-import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import NavBar from "../../components/NavBar/NavBar";
+import UserProfilePage from "../UserProfilePage/UserProfilePage";
 import "./App.css";
 
 export default function App() {
@@ -16,8 +15,7 @@ export default function App() {
         <>
           <NavBar user={ user } setUser={ setUser }/>
           <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/profile" element={<UserProfilePage user={ user } setUser={ setUser } />} />
           </Routes>
         </>
         :
@@ -26,5 +24,3 @@ export default function App() {
     </main>
   );
 }
-
-// export default App;
