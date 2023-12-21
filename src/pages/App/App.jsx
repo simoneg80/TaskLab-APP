2148c0c16d39c011fafac54bc1e0616946fbdf70
Jose <josehneves28@gmail.com>
2023-12-20 19:21:03 -0500

    test

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -12,10 +12,6 @@ export default function App() {
   const [user, setUser] = useState(getUser());
   const [tasks, setTasks] = useState([]);
 
-  function addTaskList(newTask) {
-    setTasks([...tasks, newTask]);
-  }
-
   return (
     <main className="App">
       {user ? (
