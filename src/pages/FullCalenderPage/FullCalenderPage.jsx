8ae4ab9d1375248f36b31b6f87349273cbcf9e57
Jose <josehneves28@gmail.com>
2023-12-20 19:50:48 -0500

    started to implement the calendar

--- src/pages/FullCalenderPage/FullCalenderPage.jsx
+++ src/pages/FullCalenderPage/FullCalenderPage.jsx
@@ -1,14 +1,23 @@
-// import React from 'react'
-// import FullCalendar from '@fullcalendar/react'
-// import dayGridPlugin from '@fullcalendar/daygrid' 
+import React, { useState, useEffect } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' 
 
-// export default class DemoApp extends React.Component {
-//     render() {
-//       return (
-//         <FullCalendar
-//           plugins={[ dayGridPlugin ]}
-//           initialView="dayGridMonth"
-//         />
-//       )
-//     }
-//   }
+export default function FullCalendarPage() {
+    const [calendarEvents, setCalendarEvents] = useState([]);
+
+    // Assuming you have a function to fetch folders
+    useEffect(() => {
+        fetchFolders().then(folders => {
+            const events = folders.map(folder => ({ title: folder.name, date: folder.date }));
+            setCalendarEvents(events);
+        });
+    }, []);
+
+    return (
+        <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            events={calendarEvents}
+        />
+    );
+}
